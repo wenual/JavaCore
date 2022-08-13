88bda7ea77633a03933723545a2ff669ecefbf98
Aliaksei Molchan <alexeimolchan@gmail.com>
2022-08-13 21:35:01 +0200

    Adding forgoten files to commit. Example.java and .gitignore

---
+++ src/Lessons/BookExamples/Chapter2/Example.java
@@ -0,0 +1,10 @@
+/*
+Это простая программа на Java.
+Присвоить исходному файлу имя "Example.java"
+ */
+class Example {
+    // Эта программа начинается с вызова метода main()
+    public static void main(String args[]){
+        System.out.println("Простая программа на Java.");
+    }
+}
