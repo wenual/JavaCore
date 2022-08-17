38c5df6b7ef9cecaf8d0d1b42745b6469541ceb4
Aliaksei Molchan <alexeimolchan@gmail.com>
2022-08-17 22:18:48 +0200

    Fix git add Example2.java

--- src/Lessons/BookExamples/Chapter2/Example2.java
+++ src/Lessons/BookExamples/Chapter2/Example2.java
@@ -1,2 +1,14 @@
-package Lessons.BookExamples.Chapter2;public class Example2 {
+package Lessons.BookExamples.Chapter2;
+/* Это еще один короткий пример программы.
+Присвоить исходному файлу имя "Example2.java"
+ */
+class Example2 {
+    public static void main (String args[]) {
+        int num; //в этой строке кода объявляется переменная с именем num
+        num = 100; // в этой строке кода переменной num присваевается значение 100
+        System.out.println("Это переменная num: " + num);
+        num = num * 2;
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
 }
